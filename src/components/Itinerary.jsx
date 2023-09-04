b5d6b6a5bcbb0bfa60304035c38b8c3283999ed7
Anibal Dario Zavoluk <zavolukadg@gmail.com>
2023-09-03 22:55:02 -0300

    Sprint3 Final

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -6,11 +6,13 @@ import { useState } from "react";
 import axios from "axios";
 import apiUrl from "../../apiUrl";
 
-export default function Itinerary({ itinerary, userPhoto }) {
+export default function Itinerary({ itinerary, userPhoto, name}) {
   const [active, setActive] = useState(0)
   const [heartActive, setHeartActive] = useState(0)
   const [clicks, setClicks] = useState(1)
   const [activities,setActivities] = useState([])
+
+  const duration = Math.round(itinerary.duration/60)
 
   const handleToggleActive = () => {
     setActive(!active)
@@ -41,23 +43,27 @@ export default function Itinerary({ itinerary, userPhoto }) {
     <>
       <div className="w-2/5 flex flex-col items-center p-2 m-2 bg-white">
         <p className="text-[130%] font-bold pb-2">{itinerary.name}</p>
-        <img src={itinerary.photo} alt={itinerary.name} className="" />
-        <div className="grid grid-cols-4 gap-2 w-full py-2 border border-[#c9c6c655] bg-[#e9e9e9]">
-          <div className="flex flex-col items-center font-bold ">
+        <div className="">
+          <img src={itinerary.photo} alt={itinerary.name} className=""/>
+        </div>
+        <div className="grid grid-cols-3 gap-2 w-full py-2 border border-[#c9c6c655] bg-[#e9e9e9] sm:grid-cols-4">
+          <div className="flex flex-col items-center font-bold hidden sm:flex">
             <p className="text-[2vh] text-center pb-1">User:</p>
-            <img src={userPhoto} alt="User img" className="rounded-full w-[50px] h-[50px]" />
+            <img src={userPhoto} alt="User img" className="rounded-full w-[8vh] h-[8vh]" />
+            <p className="text-[1.5vh] text-center">{name}</p>
           </div>
           <div className="flex flex-col wrap items-center">
             <p className="text-[2vh] font-bold">Hashtags:</p>
-            <p className="text-[2vh]">{itinerary.itinerary_id}</p>
+            {itinerary.tags.map((tag,index) => 
+              <p key={index} className="text-[2vh]">{tag}</p>
+            )}
           </div>
           <div className="flex flex-col items-center">
             <p className="text-[2vh] font-bold">Duration: </p>
-            <p className="text-[2vh]">{itinerary.duration}</p>
+            <p className="text-[2vh]">{duration} Hs</p>
           </div>
           <div className="flex flex-col items-center">
             <p className="text-[2vh] font-bold">Price: </p>
-            <p className="text-[2vh]">{itinerary.price}</p>
             <Bills price={itinerary.price} />
           </div>
         </div>
