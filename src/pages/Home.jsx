ff8a1d9235ab3654656e41809e09270c1bec34f9
Anibal Dario Zavoluk <zavolukadg@gmail.com>
2023-09-05 09:53:24 -0300

    Correccion rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ export default function Home() {
             <div className="flex flex-col items-center  
                 md:flex-row">
                 <div className="flex flex-col p-4 items-center md:items-start">
-                    <Anchor to='/pages/cities'>
+                    <Anchor to='/cities'>
                         <p className="text-[30px] hover:text-blue-500">Find the perfect destination!</p>
                     </Anchor>
                     <p className="text-[12px] text-justify text-gray-500 py-4 pr-5 w-[50vh] md:w-[80vh]">Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
-                    <Anchor to='/pages/cities'>
+                    <Anchor to='/cities'>
                         <button className="bg-[#4F46E5] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded flex items-center ">
                             <p className='px-5'>View More</p>
                         </button>
